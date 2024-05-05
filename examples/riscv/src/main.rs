11f28113ff590b4655f069d9ed1163ab4bd942f6
Xinyu Mu <mxy2233@outlook.com>
2024-05-05 14:07:01 +0800

    Removed useless comments.

--- examples/riscv/src/main.rs
+++ examples/riscv/src/main.rs
@@ -225,25 +225,7 @@ fn virtio_sound<T: Transport>(transport: T) {
         sound.pcm_start(output_stream_id).expect("pcm_start error");
         let music = include_bytes!("../sun_also_rises_44100Hz_u8_stereo.raw");
         info!("[sound device] music len is {} bytes.", music.len());
-        // let xfer_times = if music.len() % 64 == 0 {
-        //     music.len() / 64
-        // } else {
-        //     music.len() / 64 + 1
-        // };
-        // for i in 0..xfer_times {
-        //     let start_byte = 64 * i;
-        //     let msg = format!("pcm_xfer at xfer time {} failed", i);
-        //     // the last xfer
-        //     if i == xfer_times - 1 {
-        //         sound.pcm_xfer(output_stream_id, &music[start_byte..]).expect(&msg);
-        //         break;
-        //     }
-        //     let end_byte = 64 * (i + 1);
-        //     sound.pcm_xfer(output_stream_id, &music[start_byte..end_byte]).expect(&msg);
-
-        //     info!("xfer {} is successful.", i);
-        // }
-        match  sound.pcm_xfer(output_stream_id, &music[..=881000]) {
+        match  sound.pcm_xfer(output_stream_id, &music[..=881000]) { // xfer 10 seconds
             Ok(_) => info!("pcm_xfer OK!"),
             Err(_) => error!("pcm_xfer error"),
         }
