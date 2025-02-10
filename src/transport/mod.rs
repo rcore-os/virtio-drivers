f815726529bbbed3977a2af1a255ba33b01b1652
Ayrton Munoz <a.munoz3327@gmail.com>
2025-02-10 02:21:54 -0500

    Add DeviceTransport trait

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -15,6 +15,23 @@ use log::debug;
 pub use some::SomeTransport;
 use zerocopy::{FromBytes, Immutable, IntoBytes};
 
+/// A VirtIO device-side transport layer.
+pub trait DeviceTransport {
+    /// Gets the max size of the given queue.
+    fn max_queue_size(&mut self, queue: u16) -> u32;
+
+    /// Returns whether the transport requires queues to use the legacy layout.
+    ///
+    /// Ref: 2.6.2 Legacy Interfaces: A Note on Virtqueue Layout
+    fn requires_legacy_layout(&self) -> bool;
+
+    /// Gets the physical addresses for descriptors, driver area and device area for a given queue.
+    fn queue_get(&mut self, queue: u16) -> [PhysAddr; 3];
+
+    /// Notifies the given queue on the device.
+    fn notify(&mut self, queue: u16);
+}
+
 /// A VirtIO transport layer.
 pub trait Transport {
     /// Gets the device type.
