6917526c08aee478d5244350b22a4c179221fb7f
Nicole L <dlegare.1001@gmail.com>
2025-03-12 12:02:58 -0700

    Add safety comment to `VirtIOGpu::setup_cursor`

--- src/device/gpu.rs
+++ src/device/gpu.rs
@@ -115,12 +115,13 @@ impl<H: Hal, T: Transport> VirtIOGpu<H, T> {
         // map frame buffer to screen
         self.set_scanout(display_info.rect, SCANOUT_ID, RESOURCE_ID_FB)?;
 
-        // SAFETY: The pointer returned from `raw_slice` is guaranteed to be
-        // non-null, aligned, and a valid allocation. We store the `Dma` object
-        // in `self.frame_buffer_dma`, which prevents the allocation from being
-        // freed while `self` exists. The returned ptr borrows `self` mutably,
-        // which prevents other code from getting another reference to
-        // `frame_buffer_dma` while the returned slice is still in use.
+        // SAFETY: `Dma::new` guarantees that the pointer returned from
+        // `raw_slice` is non-null, aligned, and the allocation is zeroed. We
+        // store the `Dma` object in `self.frame_buffer_dma`, which prevents the
+        // allocation from being freed while `self` exists. The returned ptr
+        // borrows `self` mutably, which prevents other code from getting
+        // another reference to `frame_buffer_dma` while the returned slice is
+        // still in use.
         let buf = unsafe { frame_buffer_dma.raw_slice().as_mut() };
         self.frame_buffer_dma = Some(frame_buffer_dma);
         Ok(buf)
@@ -150,6 +151,11 @@ impl<H: Hal, T: Transport> VirtIOGpu<H, T> {
             return Err(Error::InvalidParam);
         }
         let cursor_buffer_dma = Dma::new(pages(size as usize), BufferDirection::DriverToDevice)?;
+
+        // SAFETY: `Dma::new` guarantees that the pointer returned from
+        // `raw_slice` is non-null, aligned, and the allocation is zeroed. The
+        // returned reference is only used within this function while
+        // `cursor_buffer_dma` is alive.
         let buf = unsafe { cursor_buffer_dma.raw_slice().as_mut() };
         buf.copy_from_slice(cursor_image);
 
