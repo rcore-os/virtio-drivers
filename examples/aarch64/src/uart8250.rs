fa7ff5208169284fdef229a7b48bbde3103d6a85
Andrew Walbran <qwandor@google.com>
2025-03-16 13:56:12 +0000

    Use safe MMIO abstraction for 8250 UART driver.

--- examples/aarch64/src/uart8250.rs
+++ examples/aarch64/src/uart8250.rs
@@ -2,27 +2,23 @@
 //! provided by crosvm, and won't work with real hardware.
 
 use core::fmt::{self, Write};
-use safe_mmio::UniqueMmioPointer;
+use safe_mmio::{fields::WriteOnly, UniqueMmioPointer};
 
 /// Minimal driver for an 8250 UART. This only implements enough to work with the emulated 8250
 /// provided by crosvm, and won't work with real hardware.
 pub struct Uart<'a> {
-    base_address: UniqueMmioPointer<'a, u8>,
+    base_address: UniqueMmioPointer<'a, WriteOnly<u8>>,
 }
 
 impl<'a> Uart<'a> {
     /// Constructs a new instance of the UART driver for a device at the given base address.
-    pub fn new(base_address: UniqueMmioPointer<'a, u8>) -> Self {
+    pub fn new(base_address: UniqueMmioPointer<'a, WriteOnly<u8>>) -> Self {
         Self { base_address }
     }
 
     /// Writes a single byte to the UART.
     pub fn write_byte(&mut self, byte: u8) {
-        // Safe because we know that the base address points to the control registers of an UART
-        // device which is appropriately mapped.
-        unsafe {
-            self.base_address.ptr_mut().write_volatile(byte);
-        }
+        self.base_address.write(byte);
     }
 }
 
